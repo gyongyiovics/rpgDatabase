65cafc66a7443302d6269d26719fd1cc4952469a
banjudy <banjudy@gmail.com>
2021-09-01 18:57:07 +0200

    Add list with random numbers

--- src/java/gitPractise.java
+++ src/java/gitPractise.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class gitPractise {
     public static void main(String[] args) {
 
+        List<Integer> numbers = new ArrayList<>();
+        loadList(numbers, 20);
+
+    }
+    public static List<Integer> loadList(List<Integer> numbers, int size) {
+        List<Integer> randomNumbers = new ArrayList<>();
+        int currentNumber = 0;
+        for (int i = 0; i < 20; i++) {
+            currentNumber = getRandomMinMax(1, 100);
+            randomNumbers.add(currentNumber);
+        }
+        return randomNumbers;
     }
 
+    public static int getRandomMinMax(int min, int max) {
+        Random rnd = new Random();
+        int random = rnd.nextInt(max + 1 - min) + min;
+        return random;
+    }
 
 }
